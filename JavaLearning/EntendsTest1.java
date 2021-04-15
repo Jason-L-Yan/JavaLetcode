c3c976651e72a0c411fbfac0f63ff333263abb92
Jason-L-Yan <licheeinfinite@163.com>
2021-04-15 15:51:50 +0800

    继承

--- JavaLearning/EntendsTest1.java
+++ JavaLearning/EntendsTest1.java
@@ -1,4 +1,26 @@
 public class EntendsTest1 {
-    
+    public static void main(String[] args){
+        Cat cat = new Cat();
+        cat.move();
+        System.out.println(cat.name);
+
+    }
     
 }
+
+
+class Animal{
+    // 名字先不封装
+    String name = "小花";
+
+    public void move(){
+        System.out.println(name + "正在移动。");
+    }
+
+}
+
+
+class Cat extends Animal{
+
+}
+
