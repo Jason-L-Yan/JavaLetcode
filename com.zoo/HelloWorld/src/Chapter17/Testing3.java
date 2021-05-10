b667d27ed2073e83177487cae8dfb96095084096
Jason-L-Yan <licheeinfinite@163.com>
2021-05-10 09:34:44 +0800

    测试

--- com.zoo/HelloWorld/src/Chapter17/Testing3.java
+++ com.zoo/HelloWorld/src/Chapter17/Testing3.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 
 public class Testing3 {
     public static void main(String[] args) {
+        Testing tet = new Testing();
         System.setProperty("file.encoding","GBK");
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
+        String str = sc.next();
         for(int i = 0; i < 10; i++){
             System.out.println("²âÊÔ£º" + str);
 
         }
-        
+        sc.close();
     }
 }
