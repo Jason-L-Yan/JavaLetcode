ba33058169bb42b6a279137c5568b9a96274821c
disheng <licheeinfinite@163.com>
2025-02-20 00:35:16 +0800

    chore: 更新VSCode设置，修改Java输出路径并调整TestOne类的输出内容

--- JavaLearning/TestOne.java
+++ JavaLearning/TestOne.java
@@ -1,5 +1,5 @@
 public class TestOne {
-    public static void main(String[] args){
-        System.out.println("Hello World");
+    public static void main(String[] args) {
+        System.out.println("Hello World22222");
     }
 }
