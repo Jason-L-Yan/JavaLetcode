f8589d9927f96a3ce2adfb77fe79f19a29e36ad3
disheng <licheeinfinite@163.com>
2025-03-18 00:55:20 +0800

    【合入描述】feat：leetcode算法 【问题单Bug】000002 【问题根因】- 【修复方案】- 【是否需补丁】-

--- src/main/java/org/example/SubsequenceSolution.java
+++ src/main/java/org/example/SubsequenceSolution.java
@@ -4,6 +4,14 @@ package org.example;
  * Leetcode 第392题，判断子序列
  */
 public class SubsequenceSolution {
+    /**
+     * 判断字符串s是否为字符串t的子序列
+     * 子序列指的是可以通过删除某些字符（也可以不删除）后，不改变字符的相对位置，得到的新字符串
+     *
+     * @param s 需要判断的子序列字符串
+     * @param t 被判断的母序列字符串
+     * @return 如果s是t的子序列，返回true；否则返回false
+     */
     public boolean isSubsequence(String s, String t) {
         int n = s.length();
         int m = t.length();
