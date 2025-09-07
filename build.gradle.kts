de8d02038980e2717a9dee7ab7ee38860d02adc0
disheng <licheeinfinite@163.com>
2025-09-07 16:58:42 +0800

    build: 更新 IntelliJ IDEA 平台依赖
    - 使用本地安装的 IntelliJ IDEA CE.app 作为平台依赖- 修改平台版本范围：自193 起至252.*
    - 请确保路径正确指向你的 IntelliJ IDEA 安装目录

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,9 @@ repositories {
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
 dependencies {
     intellijPlatform {
-        create("IC", "2025.1.4.1")
+        // 使用本地安装的IDE而不是下载
+        // 请确保路径正确指向你的IntelliJ IDEA安装目录
+        local(file("/Volumes/Samsung/Applications/IntelliJ IDEA CE.app/Contents"))
         testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
 
         // Add necessary plugin dependencies for compilation here, example:
@@ -29,7 +31,8 @@ dependencies {
 intellijPlatform {
     pluginConfiguration {
         ideaVersion {
-            sinceBuild = "251"
+            sinceBuild = "193"
+            untilBuild = "252.*"
         }
 
         changeNotes = """
