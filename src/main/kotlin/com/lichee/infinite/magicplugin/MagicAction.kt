e4f0a2ebf03a89e37b444bd0bf416198d6f12f6a
disheng <licheeinfinite@163.com>
2025-09-07 12:39:52 +0800

    feat:初始创建 MagicPlugin项目 - <resource-bundle>引入资源 - 添加项目结构和基本配置 - 实现一个简单的 MagicAction 功能 - 添加插件配置和资源文件 - 设置 Gradle 构建脚本和配置

--- src/main/kotlin/com/lichee/infinite/magicplugin/MagicAction.kt
+++ src/main/kotlin/com/lichee/infinite/magicplugin/MagicAction.kt
@@ -4,12 +4,24 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationType
+import com.lichee.infinite.magicplugin.utils.MagicPluginBundle
 
 internal class MagicAction : AnAction() {
+    
+    init {
+        val text = MagicPluginBundle.message("action.MagicAction.text")
+        val description = MagicPluginBundle.message("action.MagicAction.description")
+        templatePresentation.text = text
+        templatePresentation.description = description
+    }
+    
     override fun actionPerformed(p0: AnActionEvent) {
+        val title = MagicPluginBundle.message("delete.device.title")
+        val content = MagicPluginBundle.message("delete.device.content")
+        
         NotificationGroupManager.getInstance()
             .getNotificationGroup("MagicPluginNotification")
-            .createNotification("Magic Action", "Magic action executed!", NotificationType.INFORMATION)
+            .createNotification(title, content, NotificationType.INFORMATION)
             .notify(p0.project)
         print("Magic")
     }
---
+++ src/main/kotlin/com/lichee/infinite/magicplugin/utils/MagicActionProperties.kt
@@ -0,0 +1,15 @@
+package com.lichee.infinite.magicplugin.utils
+
+import com.intellij.DynamicBundle
+import org.jetbrains.annotations.NonNls
+import org.jetbrains.annotations.PropertyKey
+
+@NonNls
+private const val BUNDLE = "messages.MagicPluginBundle"
+
+object MagicPluginBundle : DynamicBundle(BUNDLE) {
+
+    @JvmStatic
+    fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
+        getMessage(key, *params)
+}
