ea286d6b4affbd91ec2afffd4824d8378c47586d
disheng <licheeinfinite@163.com>
2025-09-07 17:04:05 +0800

    feat: 创建 Gradle 项目构建配置
    - 添加 .gitignore 文件，配置项目忽略项
    - 创建 build.gradle.kts，设置项目构建配置
    - 添加 gradle.properties，配置 Gradle 属性
    - 创建 gradle-wrapper.properties，指定 Gradle 分发URL
    - 添加 gradlew 和 gradlew.bat，生成 Gradle 启动脚本
    - 创建 plugin.xml，配置插件信息- 添加 pluginIcon.svg，设置插件图标
    - 创建 Run IDE with Plugin.run.xml，配置运行 IDE 设置
    - 添加 settings.gradle.kts，设置项目名称

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "2.1.0"
+    id("org.jetbrains.intellij.platform") version "2.7.1"
+}
+
+group = "com.lichee.infinite"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    maven("https://maven.aliyun.com/repository/public")
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+// Configure IntelliJ Platform Gradle Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
+dependencies {
+    intellijPlatform {
+        // 使用本地安装的IDE而不是下载
+        // 请确保路径正确指向你的IntelliJ IDEA安装目录
+        local(file("/Volumes/Samsung/Applications/IntelliJ IDEA CE.app/Contents"))
+        testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
+
+        // Add necessary plugin dependencies for compilation here, example:
+        // bundledPlugin("com.intellij.java")
+    }
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "193"
+            untilBuild = "252.*"
+        }
+
+        changeNotes = """
+            Initial version
+        """.trimIndent()
+    }
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+    }
+}
